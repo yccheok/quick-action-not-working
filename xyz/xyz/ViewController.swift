0df929b19fa6724b1491e133e078cac3f4f5cd14
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2024-08-08 14:47:23 +0800

    Quick action not calling any function.

---
+++ xyz/xyz/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  xyz
+//
+//  Created by Yan Cheng Cheok on 08/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
